c230e3dbcea47af2d299658001982cb3e79ba653
Danilo Guimarães <danilo.guimaraes@managerbs.com>
2022-03-24 18:15:34 -0300

    feature: nova entidade Devedor

---
+++ NegociaFacil.Domain/Shared/Enums/TipoDocumento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegociaFacil.Domain.Shared.Enums
+{
+    public class TipoDocumento : Enumeration
+    {
+        public static readonly TipoDocumento Cpf = new(1, "CPF");
+        public static readonly TipoDocumento Cnpj = new(2, "CNPJ");
+
+        public TipoDocumento(int id, string name) : base(id, name)
+        {
+        }
+    }
+}
