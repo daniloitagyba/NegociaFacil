faf43a267cc0b653599c03f49342995aa29d8634
Danilo Itagyba <daniloitagyba@gmail.com>
2022-03-24 18:27:39 -0300

    feature: nova entidade Devedor

---
+++ NegociaFacil.Domain/Entities/Devedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegociaFacil.Domain.Entities
+{
+    public class Devedor
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public int TipoDocumento { get; set; }
+        public string Documento { get; set; }
+    }
+}
